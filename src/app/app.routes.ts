94a53909b7ead659a5f685e1f088b7fa77b046d9
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-07 15:45:12 -0300

    primeiras rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MovieAdd } from './pages/movie-add/movie-add';
+import { MovieList } from './pages/movie-list/movie-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MovieList },
+  { path: 'add', component: MovieAdd },
+];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
