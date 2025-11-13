66fda66020469ec63ac26ec8c5f25f5c64bc0c56
RitaJeveaux <ritajeveaux@gmail.com>
2025-11-13 16:46:02 -0300

    criado o auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,18 @@ import { MovieAdd } from './pages/movie-add/movie-add';
 import { MovieList } from './pages/movie-list/movie-list';
 import { Login } from './pages/login/login';
 import { NotFound } from './pages/not-found/not-found';
+import { authGuard } from './guards/auth-guard';
 
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies', component: MovieList },
-  { path: 'add', component: MovieAdd },
+  { path: 'add', component: MovieAdd, canActivate: [authGuard] },
   { path: 'login', component: Login },
   // { path: 'movies/edit/:id', component: MovieRegisterComponent },
   { path: '**', component: NotFound },
-  // , canActivate: [authGuard]
+
 ];
 
 
